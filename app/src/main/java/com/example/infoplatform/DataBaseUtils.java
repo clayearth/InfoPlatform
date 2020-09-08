e7eb81e51cd01a73a21b9e4950ccae781d2f5408
yubo dong <56866127+clayearth@users.noreply.github.com>
2020-09-08 16:48:23 +0800

    Create DataBaseUtils.java

---
+++ app/src/main/java/com/example/infoplatform/DataBaseUtils.java
@@ -0,0 +1,17 @@
+package com.example.infoplatform;
+
+
+/**
+ * 数据库工具类：连接数据库用、获取数据库数据用
+ * 相关操作数据库的方法均可写在该类
+ */
+
+public class DataBaseUtils {
+
+    private static String driver = "com.mysql.jdbc.Driver";// MySql驱动
+
+//    private static String url = "jdbc:mysql://localhost:3306/map_designer_test_db";
+
+    private static String user = "root";// 用户名
+    private static String password = "root";// 密码
+}
